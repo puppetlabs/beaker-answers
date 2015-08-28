a0e09a31db48e2da3168365d1288568dcdb2d582
Nick Lewis <nick@puppetlabs.com>
2015-08-28 15:31:14 -0700

    (APPMGMT-882) Add orchestrator answers for 2015.3
    This also updates Version40 answers to only support 2015.2, not 2015.3.

---
+++ lib/beaker-answers/versions/version20153.rb
@@ -0,0 +1,32 @@
+require 'beaker-answers/versions/version40'
+
+module BeakerAnswers
+  # This class provides answer file information for PE version 2015.3
+  #
+  # @api private
+  class Version20153 < Version40
+    # The version of PE that this set of answers is appropriate for
+    def self.pe_version_matcher
+      /\A2015\.3/
+    end
+
+    def generate_answers
+      the_answers = super
+
+      return the_answers if @options[:masterless]
+
+      master = only_host_with_role(@hosts, 'master')
+      database = only_host_with_role(@hosts, 'database')
+
+      orchestrator_db = {
+        :q_orchestrator_database_name     => answer_for(@options, :q_orchestrator_database_name),
+        :q_orchestrator_database_user     => answer_for(@options, :q_orchestrator_database_user),
+        :q_orchestrator_database_password => answer_for(@options, :q_orchestrator_database_password),
+      }
+      the_answers[master.name].merge!(orchestrator_db)
+      the_answers[database.name].merge!(orchestrator_db)
+
+      the_answers
+    end
+  end
+end
