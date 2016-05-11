d8eec98d77770c517679a628e4423fef55d0b0a5
Josh Partlow <joshua.partlow@puppetlabs.com>
2016-05-11 14:09:40 -0700

    (PE-14996) Prefer let() to instance variables
    Makes use of rspec's let method instead of instance variables for
    consistency.  Also removes extraneous 'should' from spec names.

--- spec/beaker-answers/versions/version20162_spec.rb
+++ spec/beaker-answers/versions/version20162_spec.rb
@@ -2,26 +2,23 @@ require 'spec_helper'
 require 'json'
 
 describe BeakerAnswers::Version20162 do
+  let( :ver )         { '2016.2.0' }
   let( :options )     { StringifyHash.new }
-  let( :basic_hosts ) { make_hosts( {'pe_ver' => @ver } ) }
+  let( :basic_hosts ) { make_hosts( {'pe_ver' => ver } ) }
   let( :hosts ) { basic_hosts[0]['roles'] = ['master', 'agent']
                   basic_hosts[1]['roles'] = ['dashboard', 'agent']
                   basic_hosts[2]['roles'] = ['database', 'agent']
                   basic_hosts }
-  let( :answers )     { BeakerAnswers::Answers.create(@ver, hosts, options) }
-  let( :upgrade_answers )     { BeakerAnswers::Answers.create(@ver, hosts, options.merge( {:type => :upgrade}) ) }
+  let( :answers )     { BeakerAnswers::Answers.create(ver, hosts, options) }
+  let( :answer_hash ) { answers.answers }
+  let( :upgrade_answers )     { BeakerAnswers::Answers.create(ver, hosts, options.merge( {:type => :upgrade}) ) }
 
-  before :each do
-    @ver = '2016.2'
-    @answers = answers.answers
+  it 'adds orchestrator database answers to console' do
+    expect( answer_hash['vm2'][:q_orchestrator_database_name] ).to be === 'pe-orchestrator'
+    expect( answer_hash['vm2'][:q_orchestrator_database_user] ).to be === 'Orc3Str8R'
   end
 
-  it 'should add orchestrator database answers to console' do
-    expect( @answers['vm2'][:q_orchestrator_database_name] ).to be === 'pe-orchestrator'
-    expect( @answers['vm2'][:q_orchestrator_database_user] ).to be === 'Orc3Str8R'
-  end
-
-  it 'should generate valid answers if #answer_string is called' do
+  it 'generates valid answers if #answer_string is called' do
     expect( answers.answer_string(basic_hosts[2]) ).to match /q_orchestrator_database_name=pe-orchestrator/
   end
 
@@ -30,38 +27,38 @@ describe BeakerAnswers::Version20162 do
     let( :answer_hiera ) { answers.answer_hiera }
 
     it 'should not have nil keys or values' do
-      @answers.each_pair { |k, v|
+      answer_hash.each_pair { |k, v|
         expect([k, v]).not_to include(nil)
       }
     end
 
-    it 'should have values equal to the default answers' do
-      expect(@answers["console_admin_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppet_enterpriseconsole_auth_password]
-      expect(@answers["puppet_enterprise::certificate_authority_host"]).to be === basic_hosts[0].name
-      expect(@answers["puppet_enterprise::puppet_master_host"]).to be === basic_hosts[0].name
-      expect(@answers["puppet_enterprise::console_host"]).to be === basic_hosts[1].name
-      expect(@answers["puppet_enterprise::puppetdb_host"]).to be === basic_hosts[2].name
-      expect(@answers["puppet_enterprise::database_host"]).to be === basic_hosts[2].name
-      expect(@answers["puppet_enterprise::pcp_broker_host"]).to be === basic_hosts[0].name
-      expect(@answers["puppet_enterprise::mcollective_middleware_hosts"]).to be === [basic_hosts[0].name]
-      expect(@answers["puppet_enterprise::activity_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_name]
-      expect(@answers["puppet_enterprise::activity_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_password]
-      expect(@answers["puppet_enterprise::activity_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_user]
-      expect(@answers["puppet_enterprise::classifier_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_name]
-      expect(@answers["puppet_enterprise::classifier_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_password]
-      expect(@answers["puppet_enterprise::classifier_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_user]
-      expect(@answers["puppet_enterprise::orchestrator_database_name"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_name]
-      expect(@answers["puppet_enterprise::orchestrator_database_password"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_password]
-      expect(@answers["puppet_enterprise::orchestrator_database_user"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_user]
-      expect(@answers["puppet_enterprise::puppetdb_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_name]
-      expect(@answers["puppet_enterprise::puppetdb_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_password]
-      expect(@answers["puppet_enterprise::puppetdb_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_user]
-      expect(@answers["puppet_enterprise::rbac_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_name]
-      expect(@answers["puppet_enterprise::rbac_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_password]
-      expect(@answers["puppet_enterprise::rbac_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_user]
+    it 'has values equal to the default answers' do
+      expect(answer_hash["console_admin_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppet_enterpriseconsole_auth_password]
+      expect(answer_hash["puppet_enterprise::certificate_authority_host"]).to be === basic_hosts[0].name
+      expect(answer_hash["puppet_enterprise::puppet_master_host"]).to be === basic_hosts[0].name
+      expect(answer_hash["puppet_enterprise::console_host"]).to be === basic_hosts[1].name
+      expect(answer_hash["puppet_enterprise::puppetdb_host"]).to be === basic_hosts[2].name
+      expect(answer_hash["puppet_enterprise::database_host"]).to be === basic_hosts[2].name
+      expect(answer_hash["puppet_enterprise::pcp_broker_host"]).to be === basic_hosts[0].name
+      expect(answer_hash["puppet_enterprise::mcollective_middleware_hosts"]).to be === [basic_hosts[0].name]
+      expect(answer_hash["puppet_enterprise::activity_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_name]
+      expect(answer_hash["puppet_enterprise::activity_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_password]
+      expect(answer_hash["puppet_enterprise::activity_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_user]
+      expect(answer_hash["puppet_enterprise::classifier_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_name]
+      expect(answer_hash["puppet_enterprise::classifier_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_password]
+      expect(answer_hash["puppet_enterprise::classifier_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_user]
+      expect(answer_hash["puppet_enterprise::orchestrator_database_name"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_name]
+      expect(answer_hash["puppet_enterprise::orchestrator_database_password"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_password]
+      expect(answer_hash["puppet_enterprise::orchestrator_database_user"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_user]
+      expect(answer_hash["puppet_enterprise::puppetdb_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_name]
+      expect(answer_hash["puppet_enterprise::puppetdb_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_password]
+      expect(answer_hash["puppet_enterprise::puppetdb_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_user]
+      expect(answer_hash["puppet_enterprise::rbac_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_name]
+      expect(answer_hash["puppet_enterprise::rbac_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_password]
+      expect(answer_hash["puppet_enterprise::rbac_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_user]
     end
 
-    it 'should generate valid json if #answer_hiera is called' do
+    it 'generates valid json if #answer_hiera is called' do
       expect(answer_hiera).not_to be_empty
       expect { JSON.load(answer_hiera) }.not_to raise_error
       expect(answer_hiera).to match "puppet_enterprise::puppet_master_host.*:.*#{basic_hosts[0].name}"
@@ -75,12 +72,12 @@ describe BeakerAnswers::Version20162 do
         }
       end
 
-      it 'should not include the default database passwords' do
-        expect(@answers).not_to include("puppet_enterprise::puppetdb_database_password")
-        expect(@answers).not_to include("puppet_enterprise::classifier_database_password")
-        expect(@answers).not_to include("puppet_enterprise::activity_database_password")
-        expect(@answers).not_to include("puppet_enterprise::rbac_database_password")
-        expect(@answers).not_to include("puppet_enterprise::orchestrator_database_password")
+      it 'does not include the default database passwords' do
+        expect(answer_hash).not_to include("puppet_enterprise::puppetdb_database_password")
+        expect(answer_hash).not_to include("puppet_enterprise::classifier_database_password")
+        expect(answer_hash).not_to include("puppet_enterprise::activity_database_password")
+        expect(answer_hash).not_to include("puppet_enterprise::rbac_database_password")
+        expect(answer_hash).not_to include("puppet_enterprise::orchestrator_database_password")
       end
     end
 
@@ -95,12 +92,12 @@ describe BeakerAnswers::Version20162 do
         }
       end
 
-      it 'should override the defaults when multi-level hash :answers are given' do
-        expect(@answers["puppet_enterprise::certificate_authority_host"]).to be === 'enterpriseca.vm'
+      it 'overrides the defaults when multi-level hash :answers are given' do
+        expect(answer_hash["puppet_enterprise::certificate_authority_host"]).to be === 'enterpriseca.vm'
       end
 
-      it 'should override the defaults when a :: delimited key is given' do
-        expect(@answers["puppet_enterprise::console_host"]).to be === 'enterpriseconsole.vm'
+      it 'overrides the defaults when a :: delimited key is given' do
+        expect(answer_hash["puppet_enterprise::console_host"]).to be === 'enterpriseconsole.vm'
       end
     end
   end
