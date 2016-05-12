fd1f801477424c9157a400e802880735a2ce9c59
Josh Partlow <joshua.partlow@puppetlabs.com>
2016-05-12 14:27:14 -0700

    (PE-14996) Verify answers for default, cert auth and upgrade cases
    Fixes a spec failure that occurred from
    c4556d8e455617c84ce3faf805a34fbee25bac92 given that database name/user
    were no longer present by default.  Also changes the specs to explicitly
    define hashes of expected values so we don't miss any changes
    (use_application_services wasn't being tested, for example), and hard
    codes expected constants so that we don't have false positives from both
    expectations and results coming indirectly from internal code references and
    accidentally matching.

--- spec/beaker-answers/versions/version20162_spec.rb
+++ spec/beaker-answers/versions/version20162_spec.rb
@@ -25,6 +25,43 @@ describe BeakerAnswers::Version20162 do
   context 'when generating a hiera config' do
     let( :options )      { { :format => 'hiera' } }
     let( :answer_hiera ) { answers.answer_hiera }
+    let( :default_password ) { '~!@#$%^*-/ aZ' }
+    let( :gold_role_answers ) do
+      {
+        "console_admin_password" => default_password,
+        "puppet_enterprise::use_application_services" => true,
+        "puppet_enterprise::certificate_authority_host" => basic_hosts[0].hostname,
+        "puppet_enterprise::puppet_master_host" => basic_hosts[0].hostname,
+        "puppet_enterprise::console_host" => basic_hosts[1].hostname,
+        "puppet_enterprise::puppetdb_host" => basic_hosts[2].hostname,
+        "puppet_enterprise::database_host" => basic_hosts[2].hostname,
+        "puppet_enterprise::pcp_broker_host" => basic_hosts[0].hostname,
+        "puppet_enterprise::mcollective_middleware_hosts" => [basic_hosts[0].hostname],
+      }
+    end
+    let( :gold_db_answers ) do
+      {
+        "puppet_enterprise::activity_database_name" => 'pe-activity',
+        "puppet_enterprise::activity_database_user" => 'adsfglkj',
+        "puppet_enterprise::classifier_database_name" => 'pe-classifier',
+        "puppet_enterprise::classifier_database_user" => 'DFGhjlkj',
+        "puppet_enterprise::orchestrator_database_name" => 'pe-orchestrator',
+        "puppet_enterprise::orchestrator_database_user" => 'Orc3Str8R',
+        "puppet_enterprise::puppetdb_database_name" => 'pe-puppetdb',
+        "puppet_enterprise::puppetdb_database_user" => 'mYpdBu3r',
+        "puppet_enterprise::rbac_database_name" => 'pe-rbac',
+        "puppet_enterprise::rbac_database_user" => 'RbhNBklm',
+      }
+    end
+    let( :gold_db_password_answers ) do
+      {
+        "puppet_enterprise::activity_database_password" => default_password,
+        "puppet_enterprise::classifier_database_password" => default_password,
+        "puppet_enterprise::orchestrator_database_password" => default_password,
+        "puppet_enterprise::puppetdb_database_password" => default_password,
+        "puppet_enterprise::rbac_database_password" => default_password,
+      }
+    end
 
     it 'should not have nil keys or values' do
       answer_hash.each_pair { |k, v|
@@ -32,30 +69,41 @@ describe BeakerAnswers::Version20162 do
       }
     end
 
-    it 'has values equal to the default answers' do
-      expect(answer_hash["console_admin_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppet_enterpriseconsole_auth_password]
-      expect(answer_hash["puppet_enterprise::certificate_authority_host"]).to be === basic_hosts[0].hostname
-      expect(answer_hash["puppet_enterprise::puppet_master_host"]).to be === basic_hosts[0].hostname
-      expect(answer_hash["puppet_enterprise::console_host"]).to be === basic_hosts[1].hostname
-      expect(answer_hash["puppet_enterprise::puppetdb_host"]).to be === basic_hosts[2].hostname
-      expect(answer_hash["puppet_enterprise::database_host"]).to be === basic_hosts[2].hostname
-      expect(answer_hash["puppet_enterprise::pcp_broker_host"]).to be === basic_hosts[0].hostname
-      expect(answer_hash["puppet_enterprise::mcollective_middleware_hosts"]).to be === [basic_hosts[0].hostname]
-      expect(answer_hash["puppet_enterprise::activity_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_name]
-      expect(answer_hash["puppet_enterprise::activity_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_password]
-      expect(answer_hash["puppet_enterprise::activity_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_activity_database_user]
-      expect(answer_hash["puppet_enterprise::classifier_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_name]
-      expect(answer_hash["puppet_enterprise::classifier_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_password]
-      expect(answer_hash["puppet_enterprise::classifier_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_classifier_database_user]
-      expect(answer_hash["puppet_enterprise::orchestrator_database_name"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_name]
-      expect(answer_hash["puppet_enterprise::orchestrator_database_password"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_password]
-      expect(answer_hash["puppet_enterprise::orchestrator_database_user"]). to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_orchestrator_database_user]
-      expect(answer_hash["puppet_enterprise::puppetdb_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_name]
-      expect(answer_hash["puppet_enterprise::puppetdb_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_password]
-      expect(answer_hash["puppet_enterprise::puppetdb_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_puppetdb_database_user]
-      expect(answer_hash["puppet_enterprise::rbac_database_name"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_name]
-      expect(answer_hash["puppet_enterprise::rbac_database_password"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_password]
-      expect(answer_hash["puppet_enterprise::rbac_database_user"]).to be === BeakerAnswers::Answers::DEFAULT_ANSWERS[:q_rbac_database_user]
+    it 'has the role and db password values for default install' do
+      expect(answer_hash).to eq(
+        gold_role_answers
+          .merge(gold_db_password_answers)
+      )
+    end
+
+    context "with cert auth" do
+      let(:options) do
+        {
+          :format => 'hiera',
+          :database_cert_auth => true,
+        }
+      end
+
+      it 'has the role values' do
+        expect(answer_hash).to eq(gold_role_answers)
+      end
+    end
+
+    context 'when upgrading' do
+      let(:options) do
+        {
+          :format => 'hiera',
+          :type => :upgrade,
+        }
+      end
+
+      it 'has the role and all database values' do
+        expect(answer_hash).to eq(
+          gold_role_answers
+            .merge(gold_db_answers)
+            .merge(gold_db_password_answers)
+        )
+      end
     end
 
     it 'generates valid json if #answer_hiera is called' do
