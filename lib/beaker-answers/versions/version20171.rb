ea795b85dc2bd93a4683152b6f1ce388b76e25a6
Josh Partlow <joshua.partlow@puppetlabs.com>
2016-10-27 14:47:49 -0700

    (PE-17621) Generate node_roles in 2017.1+ pe_conf
    MEEP 2.0 introduces a node_roles hash in pe.conf for classification.
    This supercedes using the puppet_enterprise host parameters as a de
    facto classification.  The Version20171 class maps existing beaker role
    aliases to the new role strings used for classification in pe.conf.
    
    So, from a MEEP 1.0 version pe.conf such as:
    
      {
        "puppet_enterprise::puppet_master_host": "master.net"
        "puppet_enterprise::puppetdb_host": "puppetdb.net"
        "puppet_enterprise::console_host": "console.net"
      }
    
    You now have a 2.0 version that looks like:
    
      {
        "node_roles": {
          "pe_role::split::primary_master": ["master.net"]
          "pe_role::split::puppetdb": ["puppetdb.net"]
          "pe_role::split::console": ["console.net"]
        }
      }
    
    See the pe_manager and pe_infrastructure modules for more details.

--- lib/beaker-answers/versions/version20171.rb
+++ lib/beaker-answers/versions/version20171.rb
@@ -9,5 +9,75 @@ module BeakerAnswers
     def self.pe_version_matcher
       /\A2017\.1/
     end
+
+    # This lists various aliases for different PE components that might
+    # be installed on a host.
+    #
+    # The first entry in the list is the canonical name for the
+    # component from the PuppetX::Puppetlabs::Meep::Config.pe_components()
+    # from the pe_infrastructure module.
+    #
+    # Secondary aliases are the various working names that have been used
+    # with beaker in CI to establish that a host has that component
+    # installed on it.
+    COMPONENTS = [
+      ["primary_master", "master"],
+      ["primary_master_replica"],
+      # the 'database' alias will be troublesome if/when we begin
+      # breaking out a managed database (as opposed to external)
+      # in future PE layouts.
+      ["puppetdb", "database"],
+      ["console", "classifier", "dashboard"],
+      ["compile_master"],
+      ["mco_hub", "hub"],
+      ["mco_broker", "spoke"],
+    ]
+
+    # @param host_role_aliases [Array<String>] list of strings to search for in
+    #   each host's role array
+    # @return [Array<String>] of hostnames from @hosts that include at least
+    #   one of the passed aliases
+    def all_hosts_with_component(host_role_aliases)
+      hosts = @hosts.select do |h|
+        !(Array(h['roles']) & host_role_aliases).empty?
+      end
+      hosts.map { |h| h.hostname }
+    end
+
+    # This used to generate the profile host parameters, but now generates a MEEP
+    # 2.0 node_roles hash mapping roles -> node certs based on the same host and
+    # role information.
+    def hiera_host_config
+      pe_conf = {
+        "node_roles" => {}
+      }
+      hosts_by_component = {}
+
+      COMPONENTS.each do |aliases|
+        meep_component_name = aliases.first
+        hosts_by_component[meep_component_name] = all_hosts_with_component(aliases)
+      end
+
+      # Reject console and puppetdb lists if they are the same as master
+      hosts_by_component.reject! do |component,hosts|
+        ["puppetdb", "console"].include?(component) &&
+          hosts_by_component["primary_master"].sort == hosts.sort
+      end
+
+      # Which is also sufficient to determine our architecture at the moment
+      architecture = (hosts_by_component.keys & ["puppetdb", "console"]).empty? ?
+        "monolithic" :
+        "split"
+
+      hosts_by_component.reduce(pe_conf) do |conf,entry|
+        component, hosts = entry
+        if !hosts.empty?
+          conf["node_roles"]["pe_role::#{architecture}::#{component}"] = hosts
+        end
+        conf
+      end
+
+      pe_conf
+    end
   end
 end
---
+++ spec/beaker-answers/versions/version20171_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+require 'json'
+
+describe BeakerAnswers::Version20171 do
+  let( :ver )         { '2017.1.0' }
+  let( :options )     { StringifyHash.new }
+  let( :basic_hosts ) { make_hosts( {'pe_ver' => ver } ) }
+  let( :hosts ) { basic_hosts[0]['roles'] = ['master', 'agent']
+                  basic_hosts[1]['roles'] = ['dashboard', 'agent']
+                  basic_hosts[2]['roles'] = ['database', 'agent']
+                  basic_hosts }
+  let( :answers )     { BeakerAnswers::Answers.create(ver, hosts, options) }
+  let( :answer_hash ) { answers.answers }
+
+  context 'when generating a hiera config' do
+    context 'for a monolithic install' do
+      let( :basic_hosts ) { make_hosts( {'pe_ver' => ver }, 1 ) }
+      let( :hosts ) { basic_hosts[0]['roles'] = ['master', 'agent', 'dashboard', 'database']
+                      basic_hosts }
+      let( :gold_role_answers ) do
+        {
+          "console_admin_password" => default_password,
+          "node_roles" => {
+            "pe_role::monolithic::primary_master" => [basic_hosts[0].hostname],
+          }
+        }
+      end
+
+      it "accepts overrides to node_roles from beaker conf answers"
+
+      include_examples 'pe.conf'
+    end
+
+    context 'for a split install' do
+      let( :gold_role_answers ) do
+        {
+          "console_admin_password" => default_password,
+          "node_roles" => {
+            "pe_role::split::primary_master" => [basic_hosts[0].hostname],
+            "pe_role::split::console" => [basic_hosts[1].hostname],
+            "pe_role::split::puppetdb" => [basic_hosts[2].hostname],
+          }
+        }
+      end
+
+      include_examples 'pe.conf'
+    end
+  end
+end
