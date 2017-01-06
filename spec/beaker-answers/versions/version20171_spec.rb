6b6de699f35c7b96fb0fab4fb11c4e57085fff77
Josh Partlow <joshua.partlow@puppetlabs.com>
2017-01-05 16:00:18 -0800

    (PE-18282) Add agent_platforms array to 2017.1 answer files
    This provides the list of agent platform classes based on all host
    platform settings. As of 2017.1 this is set in pe.conf.

--- spec/beaker-answers/versions/version20171_spec.rb
+++ spec/beaker-answers/versions/version20171_spec.rb
@@ -6,8 +6,11 @@ describe BeakerAnswers::Version20171 do
   let( :options )     { StringifyHash.new }
   let( :basic_hosts ) { make_hosts( {'pe_ver' => ver } ) }
   let( :hosts ) { basic_hosts[0]['roles'] = ['master', 'agent']
+                  basic_hosts[0]['platform'] = 'el-6-x86_64'
                   basic_hosts[1]['roles'] = ['dashboard', 'agent']
+                  basic_hosts[1]['platform'] = 'el-6-x86_64'
                   basic_hosts[2]['roles'] = ['database', 'agent']
+                  basic_hosts[2]['platform'] = 'ubuntu-14.04-amd64'
                   basic_hosts }
   let( :answers )     { BeakerAnswers::Answers.create(ver, hosts, options) }
   let( :answer_hash ) { answers.answers }
@@ -16,13 +19,15 @@ describe BeakerAnswers::Version20171 do
     context 'for a monolithic install' do
       let( :basic_hosts ) { make_hosts( {'pe_ver' => ver }, 1 ) }
       let( :hosts ) { basic_hosts[0]['roles'] = ['master', 'agent', 'dashboard', 'database']
+                      basic_hosts[0]['platform'] = 'el-7-x86_64'
                       basic_hosts }
       let( :gold_role_answers ) do
         {
           "console_admin_password" => default_password,
           "node_roles" => {
             "pe_role::monolithic::primary_master" => [basic_hosts[0].hostname],
-          }
+          },
+          "agent_platforms" => match_array(['el_7_x86_64'])
         }
       end
 
@@ -40,7 +45,8 @@ describe BeakerAnswers::Version20171 do
             "pe_role::split::primary_master" => [basic_hosts[0].hostname],
             "pe_role::split::console" => [basic_hosts[1].hostname],
             "pe_role::split::puppetdb" => [basic_hosts[2].hostname],
-          }
+          },
+          "agent_platforms" => match_array(['el_6_x86_64', 'ubuntu_1404_amd64'])
         }
       end
 
