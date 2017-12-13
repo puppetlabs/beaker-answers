f494e61ebe87da5ab59f29fcff66ace7be71ca7c
Brandon High <brandon.high@puppet.com>
2017-12-13 11:07:09 -0800

    (coverage) Actually run simplecov
    Prior to this commit simplecov was included in the gemspec but never
    run.
    This commit updates the spec_helper.rb to run simplecov.
    Also sets the `minimum_coverage` to 90%, since we're already at 94%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 require 'simplecov'
+SimpleCov.start
+SimpleCov.minimum_coverage 90
+SimpleCov.minimum_coverage_by_file 80
+
 require 'beaker-answers'
 require 'helpers'
 require 'shared/context.rb'
