c1ddfd5def62cdfc544278225f917879f5fd3450
Brandon High <brandon.high@puppet.com>
2017-12-13 11:47:12 -0800

    (lint) Fix Style/ConditionalAssignment that Rubocop wouldn't ignore
    Rubocop would not let this one go, so I fixed it. It's in verion30,
    so whatever.

--- lib/beaker-answers/versions/version30.rb
+++ lib/beaker-answers/versions/version30.rb
@@ -205,18 +205,16 @@ module BeakerAnswers
       dashboard   = masterless ? nil : only_host_with_role(@hosts, 'dashboard')
       master      = masterless ? nil : only_host_with_role(@hosts, 'master')
       @hosts.each do |h|
-        if (@options[:type] == :upgrade) && h[:pe_ver] =~ /\A3.0/
-          # 3.0.x to 3.0.x should require no answers
-          the_answers[h.name] = {
-            q_install: answer_for(@options, :q_install, 'y'),
-            q_install_vendor_packages: answer_for(@options, :q_install_vendor_packages, 'y')
-          }
-        else
-          the_answers[h.name] = host_answers(h, master, database, dashboard, @options)
-        end
-        if the_answers[h.name] && h[:custom_answers]
-          the_answers[h.name] = the_answers[h.name].merge(h[:custom_answers])
-        end
+        the_answers[h.name] =
+          if (@options[:type] == :upgrade) && h[:pe_ver] =~ /\A3.0/
+            {
+              q_install: answer_for(@options, :q_install, 'y'),
+              q_install_vendor_packages: answer_for(@options, :q_install_vendor_packages, 'y')
+            }
+          else
+            host_answers(h, master, database, dashboard, @options)
+          end
+        the_answers[h.name] = the_answers[h.name].merge(h[:custom_answers]) if the_answers[h.name] && h[:custom_answers]
         h[:answers] = the_answers[h.name]
       end
       the_answers
