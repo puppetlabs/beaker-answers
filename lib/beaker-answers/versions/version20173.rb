0246d2180b4407018b8aa130700f25c0df40b20d
Brandon High <brandon.high@puppet.com>
2019-10-25 12:10:31 -0700

    (PE-27513) Stop requiring update for every Y release
    This commit updates `Version20193` to be `Version20193to9999` by
    updating the regex to match just about any future date-based version.
    It also removes all the redundant versions and their specs and replaces
    them with a dynamic array of specs on the `Verions20193to9999` spec.

--- lib/beaker-answers/versions/version20173.rb
+++ lib/beaker-answers/versions/version20173.rb
@@ -1,13 +1,13 @@
 require 'beaker-answers/versions/version20172'
 
 module BeakerAnswers
-  # This class provides answer file information for PE version 2017.3
+  # This class provides answer file information for PE version 2017.3 AND BEYOND
   #
   # @api private
-  class Version20173 < Version20172
+  class Version20173to9999 < Version20172
     # The version of PE that this set of answers is appropriate for
     def self.pe_version_matcher
-      /\A2017\.3/
+      /\A(2017\.3|2018\.[1-2]|2019\.[0-9]|20[2-9][1-9]\.[0-9]|[2-9][0-9]{3}\.[0-9])/
     end
   end
 end
